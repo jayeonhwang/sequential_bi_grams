4e716be718a40cf86e75fe19786c0fee329478ee
Jayeon Hwang <jayeon512@gmail.com>
2024-04-17 17:40:10 -0500

    add bigrams array

--- index.rb
+++ index.rb
@@ -1,11 +1,17 @@
+# Sequential Bi-grams
+# Given a string of words, create a set of every sequential bi-gram.  Bigrams are two-word combinations of the text, i.e. “Charlie rocks”.
+ 
+
 words = "Make a killer impression on whoever you’re meeting."
-
 word = words.split(' ')
-
 index = 0
 
+bigrams = []
+
 while word.length > index
-  puts "#{word[index]} #{word[index+1]}"
+bigram = "#{word[index]} #{word[index+1]}"
+  bigrams << bigram
   index += 1
 end
 
+p bigrams
